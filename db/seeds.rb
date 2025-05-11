0e056ed833543b1e9c5df40b4b9205d94f826099
Javier Rosales <javier.rb_96@hotmail.com>
2025-05-11 15:49:51 +0200

    Add dataset seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,57 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+data = JSON.parse(File.read('db/streams_data.json'))
+
+p 'destroy_all'
+App.destroy_all
+Movie.destroy_all
+TvShow.destroy_all
+Channel.destroy_all
+
+p 'movies'
+data['movies'].each do |movie_data|
+  movie = Movie.create!(movie_data.slice('original_title', 'year', 'duration_in_seconds'))
+  movie_data['availabilities'].each do |availability|
+    app = App.find_or_create_by!(name: availability['app'])
+    movie.availabilities.create!(app: app, market: availability['market'])
+  end
+end
+
+p 'tv_shows'
+data['tv_shows'].each do |tv_data|
+  tv = TvShow.create!(tv_data.slice('original_title', 'year', 'duration_in_seconds'))
+  tv_data['availabilities'].each do |availability|
+    app = App.find_or_create_by!(name: availability['app'])
+    tv.availabilities.create!(app: app, market: availability['market'])
+  end
+
+  tv_data['seasons'].each do |season_data|
+    season = tv.seasons.create!(season_data.slice('original_title', 'number', 'year', 'duration_in_seconds'))
+    season_data['availabilities'].each do |availability|
+      app = App.find_or_create_by!(name: availability['app'])
+      season.availabilities.create!(app: app, market: availability['market'])
+    end
+  end
+
+  tv_data['episodes'].each { |ep| tv.episodes.create!(ep) }
+end
+
+p 'channels'
+data['channels'].each do |channel_data|
+  channel = Channel.create!(channel_data.slice('original_title', 'year', 'duration_in_seconds'))
+  channel_data['availabilities'].each do |availability|
+    app = App.find_or_create_by!(name: availability['app'])
+    channel.availabilities.create!(app: app, market: availability['market'])
+  end
+
+  channel_data['channel_programs'].each do |program_data|
+    program = channel.channel_programs.create!(program_data.slice('original_title', 'year', 'duration_in_seconds'))
+    program_data['schedule'].each { |sc| program.schedules.create!(sc) }
+    program_data['availabilities'].each do |availability|
+      app = App.find_or_create_by!(name: availability['app'])
+      program.availabilities.create!(app: app, market: availability['market'])
+    end
+  end
+end
